89f209b945c5fa2baf077395ec8ba6769490ab4a
F4RAN <parsa.hassani30@gmail.com>
2024-06-11 21:56:33 +0330

    second week project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-
 fn main() {
-    let file = File::open("non_existent_file.txt");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => {
+            panic!("No arguments provided");
+        }
+        2 => {
+            println!("Provided path by user: {}", args[1]);
+        }
+        _ => {
+            panic!("Too many arguments provided");
+        }
+    }
+    let path = &args[1];
+
+    let file = File::open(path);
     let file = match file {
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -16,6 +30,7 @@ fn main() {
     };
 
     let reader = BufReader::new(file);
+    println!("Story content:");
     for line in reader.lines() {
         match line {
             Ok(line) => println!("{}", line),
